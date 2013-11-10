92f2d12582efecf01339332e7eeaee328c255974
muki <filipdobranic@gmail.com>
2013-11-10 20:04:19 +0100

    Fixed scroller bug
    Alignment isn't shaky, everything runs smoother

--- js/scroller.js
+++ js/scroller.js
@@ -11,32 +11,31 @@
 	$(window).scroll(function() {
 	    clearTimeout($.data(this, 'scrollTimer'));
 	    $.data(this, 'scrollTimer', setTimeout(function() {	        
-
 	        // calculate diff absolute value
 	        diff = Math.abs(parseInt($('#rightColumn').css('top').split('px')[0]) % $(window).height());
 	        console.log('diff = ' + diff);
 	        
 	        // if diff is smaller than half of screen
-	        if (diff <= $(window).height()/2) {
+	        if (diff != 0 && diff < $(window).height()/2) {
 	        	console.log($(window).scrollTop() + diff);
-	        	$('html body').animate({
+	        	$('body').animate({
 	        		scrollTop: $(window).scrollTop() + diff
-	        	}, 1000, function() {
-	        		$('html body').stop(true, true);
+	        	}, 500, 'swing', function() {
+	        		$('body').stop(true, true);
 	        	});
 	        }
 	        // if diff is larger than half of screen
-	        else {
+	        else if (diff > $(window).height()/2) {
 	        	console.log($(window).scrollTop() - diff);
-	        	$('html body').animate({
+	        	$('body').animate({
 	        		scrollTop: $(window).scrollTop() - ($(window).height()-diff)
-	        	}, 1000, function() {
-	        		$('html body').stop(true, true);
+	        	}, 500, 'swing', function() {
+	        		$('body').stop(true, true);
 	        	});
 	        }
 	        
 	        console.log("Haven't scrolled in 500ms!");
-	    }, 500));
+	    }, 250));
 	});
 	
 //}
